12443a88a30cbe595b5748d22fc1210b52be5bfb
Chantikka Riffka <chantikkariffka@gmail.com>
2023-03-07 12:56:59 +0700

    update

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    public function index(Request $request){
+        return view('welcome');
+    }
+    public function about(Request $request){
+        return view('about');
+    }
+}
